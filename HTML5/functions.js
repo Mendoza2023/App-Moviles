04fae5920220fd02689daa8edac47098c93400ca
Marcos Mendoza <126982893+Mendoza2023@users.noreply.github.com>
2024-04-24 16:03:32 -0600

    Delete HTML5 directory

--- HTML5/functions.js
+++
@@ -1,5 +0,0 @@
-function greeting() {
-    const name = document.getElementById("name").value;
-    const age = document.getElementById("age").value;
-    alert("Hello  " + name + ",   you have " + age + "  years old")
-}
